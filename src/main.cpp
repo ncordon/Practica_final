4ce924abe28c5457811a3e5e8c68d953f5e2c9e0
David Charte <fdavidcl@outlook.com>
2014-01-28 18:17:00 +0100

    Ajustando main de ruta a guion

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include "pais.h"
 #include "creadorimagenes.h"
 
-int main(int argc, char* argv[]){
-    if (argc!=9){
-        cout << "Los parametros son :" << endl;
+int main(int argc, char* argv[]) {
+    if (argc != 8) {
+        cout << "Los parametros son:" << endl;
         cout << "1. El fichero de países" << endl;
         cout << "2. La imagen del mapa de fondo" << endl;
         cout << "3. El directorio de banderas" << endl;
@@ -12,21 +12,26 @@ int main(int argc, char* argv[]){
         cout << "5. La imagen del avión a emplear" << endl;
         cout << "6. La máscara para el pegado de avión" << endl;
         cout << "7. La ruta a mostrar" << endl;
-        cout << "8. El nombre del fichero de salida" << endl;
         return -1;
     }
 
     // Leemos la imagen del mapa y del avión de memoria
     Imagen avion,mapa;
     mapa.leer(argv[2]);
-    avion.leer(argv[5],argv[6]);
-    string objetivo(argv[7]), dir(argv[3]);
+    avion.leer(argv[5], argv[6]);
+    string objetivo(argv[7]), dirbanderas(argv[3]);
+
     // Fichero de rutas
     ifstream f_rutas(argv[4]);
     // Fichero de paises
     ifstream f_paises(argv[1]);
+
+    if (!f_rutas || !f_paises) {
+        cerr << "Error: No se ha encontrado el archivo de rutas o de países" << endl;
+        return -2;
+    }
     
-    Paises almacenp(dir);
+    Paises almacenp(dirbanderas);
     AlmacenRutas almacenr;
     
     // Leo los almacenes de rutas y de países
@@ -36,6 +41,7 @@ int main(int argc, char* argv[]){
     CreadorImagenes impresor_rutas(mapa, avion);
     Imagen maparuta(impresor_rutas.muestraRuta(almacenr[objetivo], almacenp));
 
-    maparuta.escribir(argv[8],"PPM");
+    string nombresalida(argv[7]);
+    nombresalida += ".ppm";
+    maparuta.escribir(nombresalida.c_str(), "PPM");
 }
-    
